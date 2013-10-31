3d7241231813b3f8498627957a1de348ad00d6b9
Nathan Rajlich <nathan@tootallnate.net>
2013-10-30 17:04:41 -0700

    fallback: clean up the GNODE_ENTRY_POINT env variable

--- fallback.js
+++ fallback.js
@@ -1,8 +1,12 @@
 
+// grab a reference to the entry point script, then clean up the env
+var entryPoint = process.env.GNODE_ENTRY_POINT;
+delete process.env.GNODE_ENTRY_POINT;
+
 // load the custom `.js` ES6 Generator compiler
 require('./index.js');
 
 // replace `process.argv[1]` with the expected path value,
 // and re-run Module.runMain()
-process.argv[1] = require('path').resolve(process.env.GNODE_ENTRY_POINT);
+process.argv[1] = require('path').resolve(entryPoint);
 require('module').runMain();
