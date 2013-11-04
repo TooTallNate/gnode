2e16f427cae2b82072528a061dba59f5a0d8e91b
Nathan Rajlich <nathan@tootallnate.net>
2013-11-03 21:47:00 -0800

    test: remove dead code

--- test/cli/check.js
+++ test/cli/check.js
@@ -8,7 +8,5 @@ assert('GeneratorFunction' == gen.constructor.name);
 function *gen () {}
 
 var g = gen();
-//console.error(g.constructor + '');
-//assert('Generator' == g.constructor.name);
 assert('function' == typeof g.next);
 assert('function' == typeof g.throw);
